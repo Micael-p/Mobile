7be1d8d81ea7c184522eab1d1276818c3ef892fd
Micael-p <micaelpessoa5@gmail.com>
2025-09-08 10:03:49 -0300

    Update App.js

--- Exercícios/08-09-25/2-/App.js
+++ Exercícios/08-09-25/2-/App.js
@@ -1,1 +1,13 @@
+import { Text, View, TouchableOpacity, Button } from 'react-native';
+import styles, { saudacao } from './style.js'
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+       <Text style={styles.buttonText}>Clique para saudação</Text>
+      </TouchableOpacity> 
+      <Text>{saudacao('Micael :D')}</Text>
+    </View>
+  );
+}
