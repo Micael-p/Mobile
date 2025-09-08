7dec5711c225eee993090e22575465525b39e4a5
Micael-p <micaelpessoa5@gmail.com>
2025-09-08 10:10:01 -0300

    Update help.js

--- Importando_a_função_com_e_sem_default/help.js
+++ Importando_a_função_com_e_sem_default/help.js
@@ -1,1 +1,11 @@
+export default function saudacao(nome) {
+  return `Olá, ${nome}`;
+}
 
+export function toUpper(texto){
+  return texto.toUpperCase();
+}
+
+export function toLower(texto){
+  return texto.toLowerCase();
+}
