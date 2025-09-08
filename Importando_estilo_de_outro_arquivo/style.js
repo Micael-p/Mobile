ba23d7ed0702d56fef38b73fcf909de6ef14f5e8
Micael-p <micaelpessoa5@gmail.com>
2025-09-08 08:43:30 -0300

    Create style.js

---
+++ Importando_estilo_de_outro_arquivo/style.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  button: {
+    backgroundColor: "#4CAF50",
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
