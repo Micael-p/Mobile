73ce46b339c78f9971fdd12019a96de416137cf6
Micael-p <micaelpessoa5@gmail.com>
2025-09-08 10:09:42 -0300

    Update App.js

--- Importando_a_função_com_e_sem_default/App.js
+++ Importando_a_função_com_e_sem_default/App.js
@@ -1,1 +1,12 @@
+import { Text, View, StyleSheet } from 'react-native';
+import saudacao, { toUpper, toLower } from './help.js'
 
+export default function App() {
+  return (
+    <View>
+      <Text>{saudacao('Micael :D')}</Text>
+      <Text>{toUpper('caixa alta')}</Text>
+      <Text>{toLower('CAIXA BAIXA')}</Text>
+    </View>
+  );
+}
