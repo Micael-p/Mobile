6778a7a0171e77f0d2bf465634a45a950b8552b9
Micael-p <micaelpessoa5@gmail.com>
2025-09-08 10:06:58 -0300

    Update App.js

--- Importando_a_função_de_outro_arquvo/App.js
+++ Importando_a_função_de_outro_arquvo/App.js
@@ -1,1 +1,15 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { subtrair, multiplicar } from './math.js'
 
+export default function App() {
+  return (
+    <View>
+      <Text>Subtração: {subtrair(5, 7)} </Text>
+      <Text>Multiplicação: {multiplicar(6, 5)} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
