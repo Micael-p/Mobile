181c883adde6c7b14ea4bf4eda75f8815377d70d
Micael-p <micaelpessoa5@gmail.com>
2025-09-15 10:04:04 -0300

    Create style.js

---
+++ Exercícios/15-09-25/1-/style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from "react-native"
+
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    padding: 8,
+  },
+  login: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    margin: 20,
+  },
+  input1: {
+    backgroundColor:'white',
+    margin:5,
+    padding:5,
+  },
+})
