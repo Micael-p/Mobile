a26e24498e3df0f32d9cd7054993e39893bfbe70
Micael-p <micaelpessoa5@gmail.com>
2025-09-22 09:28:17 -0300

    Create Style.js

---
+++ Login_completo_com_imagem/Style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+container:{
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#d9d9d9',
+},
+logo: {
+  width: 100,
+  height: 90,
+  marginBottom: 20,
+},
+input: {
+  borderWidth: 1,
+  borderColor: '#999',
+  borderRadius: 5,
+  padding: 10,
+  marginBottom: 10,
+  width: '80%',
+  backgroundColor: '#fff',
+  placeholderTextColor: 'black',
+},
+botao: {
+  marginTop: 10,
+},  
+
+})
